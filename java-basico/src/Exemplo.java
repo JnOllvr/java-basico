1206eb39ac1d2260dadf6d380007eb23e299d6ee
JnOllvr <tallmagician2005@gmail.com>
2025-03-18 11:04:18 -0300

    for--

---
+++ java-basico/src/Exemplo.java
@@ -0,0 +1,8 @@
+public class Exemplo {
+    public static void main(String[] args) {
+        for (int carneiros =1; carneiros <=20; carneiros ++){
+            System.out.println("Contagem de carneiros " + carneiros);
+        }
+        System.out.println("Dormiu!");
+    }
+}
